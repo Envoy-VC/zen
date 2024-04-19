425448da3b50f186cc4c5574dbe325b4df228d9b
Vedant Chainani <vedantchainani1084@gmail.com>
2024-04-19 12:37:27 +0530

    chore: add dark mode provider and hook

--- apps/www/src/main.tsx
+++ apps/www/src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { ThemeProvider } from '~/providers';
+
 import App from './App.tsx';
 
 const Root = (
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme='dark' storageKey='theme'>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
